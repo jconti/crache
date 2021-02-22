8e4426692c01befdbd02e9d0b16fafa659f9db03
John <jhconti@icloud.com>
2021-02-22 08:55:39 -0700

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject crache "1.0.0-rc1"
-  :description "Redis-backed caching and memoization, following core.cache and core.memoize"
+(defproject org.clojars.jconti/crache "1.0.1-rc1"
+  :description "Redis-backed caching and memoization, forked from http://github.com/strongh/crache"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "http://github.com/strongh/crache"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.taoensso/carmine "2.7.0"]
-                 [org.clojure/core.cache "0.6.4"]
-                 [org.clojure/core.memoize "0.5.6"]])
+  :url "http://github.com/jconti/crache"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.taoensso/carmine "3.1.0"]
+                 [org.clojure/core.cache "1.0.207"]
+                 [org.clojure/core.memoize "1.0.236"]])
